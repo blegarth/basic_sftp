9bf73d6e1a7d9e91d8042bfe81e57b5d034c745a
Brian Legarth <legarth8@gmail.com>
2020-05-16 05:06:37 -0400

    Still working on the requirements to make the pip install work immediately

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'pysftp', ]
+requirements = ['Click>=7.0', ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner', 'pysftp', ]
 
 test_requirements = ['pytest>=3', ]
 
